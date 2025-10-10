86bb417d6297487f5022725b9346e40a5e416ec4
horthheng <horth.heng2025@gmail.com>
2025-10-10 14:41:43 +0700

    add author

--- src/author/author.controller.ts
+++ src/author/author.controller.ts
@@ -7,18 +7,42 @@ import {
   Param,
   Body,
   ParseIntPipe,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { diskStorage } from 'multer';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller('authors')
 export class AuthorController {
   constructor(private authorService: AuthorService) {}
 
   @Post()
-  create(@Body() dto: CreateAuthorDto) {
-    return this.authorService.create(dto);
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads/author',
+        filename: (_, file, cb) => {
+          const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + extname(file.originalname);
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  create(
+    @UploadedFile() file?: Express.Multer.File,
+    @Body() body?: any, // Use `any` because FormData cannot be mapped to DTO automatically
+  ) {
+    const data: CreateAuthorDto = {
+      name: body.name,
+      bio: body.bio,
+      image: file?.filename,
+    };
+    return this.authorService.create(data);
   }
 
   @Get()
@@ -31,10 +55,31 @@ export class AuthorController {
     return this.authorService.findOne(id);
   }
 
-  @Put(':id')
-  update(@Param('id', ParseIntPipe) id: number, @Body() dto: UpdateAuthorDto) {
-    return this.authorService.update(id, dto);
+@Put(':id')
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './uploads/author',
+        filename: (_, file, cb) => {
+          const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1e9) + extname(file.originalname);
+          cb(null, uniqueName);
+        },
+      }),
+    }),
+  )
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @UploadedFile() file?: Express.Multer.File,
+    @Body() body?: any,
+  ) {
+    const data: Partial<CreateAuthorDto> = {
+      name: body.name,
+      bio: body.bio,
+      image: file?.filename,
+    };
+    return this.authorService.update(id, data);
   }
+
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
